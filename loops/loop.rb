90900e3992809e81f8c98c8caccc76dbe3910f84
makena <roselynemakena@gmail.com>
2017-08-19 08:19:03 +0000

    Add #loops

---
+++ loops/loop.rb
@@ -0,0 +1,27 @@
+require 'awesome_print'
+
+#Iterator variable - i
+#print 1-10
+
+
+#while loop
+i=0
+fname ="makena"
+while i<=10
+
+	fname="Rosa"
+	puts "i is now #{i}"
+
+	i+=1
+end
+
+puts "Final vlu of fname i #{fname}"
+
+#until loop
+j = 0
+until j>5
+	puts "j is less than 5"
+	j+=1
+end
+
+puts "j is finally greater than 5"
