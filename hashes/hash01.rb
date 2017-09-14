47f0bc8eb9b9d7c22b163153bb3180b1ba0989a5
Makena <roselynemakena@gmail.com>
2017-09-14 17:36:45 +0300

    #modify hash01

--- hashes/hash01.rb
+++ hashes/hash01.rb
@@ -61,9 +61,81 @@ puts "------------------------"
 a = [1,4,2,3,5]
 puts a.sort
 
-puts "------------------------"
+puts "--------SORTING HASH----------------"
 
 
 ha = {:one => 1, :two => 2, :three => 3, :four => 4, :five => 5, :six => 6 , :seven => 7, :eight => 8}
 puts ha.sort
+
+=begin
+Each time a string is created, an object is created for it. But a symbpl points to the same object in memory.
+
+=end
+
+puts 'name'.object_id
+puts 'name'.object_id
+puts 'name'.object_id
+
+
+puts "---------SYMBOLS---------------"
+
+
+puts :name.object_id
+puts :name.object_id
+puts :name.object_id
+
+# Creating a hash with a default key values
+# default valuue
+alright = Hash.new("default")
+
+puts alright["ok"]
+puts "------------------------"
+
+
+# Return default for string key, otherwise return not string
+
+h = {"one"=>1, "two"=>2, 3=>3, 4=>4}
+
+# h.each {}
+
+puts "------------------------"
+
+
+#create a hash with default key depending on the value - if the value is string->string, else "other"
+
+hasher = Hash.new{|h, k| (k.respond_to? :to_str) ? "string" : "other"}
+
+puts hasher[1]
+
+#word count string into a hash
+puts "------------Counting words in a hash------------"
+
+
+string_hash = "Once upon a time in a time where grass was greem. Time would tell why it was like that."
+
+counted_hash = Hash.new(0)
+
+string_hash.split(/\W+/).each {|word| counted_hash[word.downcase] +=1 }
+
+puts counted_hash
+
+puts "-----------Adding values to hash-------------"
+
+h["one"] = "one replaced"
+
+puts h
+
+puts "-----------keys in hash are flattened and cannot be edited-------------"
+
+key = "superkey"
+
+h[key] = "Super Value"
+
+puts h
+
+puts "--------Iterating through hash----------------"
+
+h.each_pair {|k,v| puts"#{k} maps to #{v}"}
+
+
 
