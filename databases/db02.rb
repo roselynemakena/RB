3255efd0b9c5bce8e77daa404d4ee5324e11f01a
Makena <roselynemakena@gmail.com>
2017-11-30 17:27:58 +0300

    #db active record validation

--- databases/db02.rb
+++ databases/db02.rb
@@ -5,10 +5,11 @@ require 'mysql2'
 class Article <ActiveRecord::Base
 
 	has_many :comments
-
+	validates_presence_of :title
 end
 class Comment <ActiveRecord::Base
 
+validates_presence_of :comment_title
 belongs_to :article
 
 end
@@ -28,9 +29,29 @@ end
 
 connect_to_db
 
-article = Article.new(:title=>"First article", :description => "I once had a dream, that all the people in the world")
+article = Article.new(:title=>"Article super", :description => "In the end, it was well")
 article.save
+puts article.errors['title']
 
 # comment = Comment.new(:comment_title => "Great article", :comment_body => "Well written, write again soon!")
-comment = {:comment_title => "Great article", :comment_body => "Well written, write again soon!"}
+if article.save
+comment = {:comment_title => "new", :comment_body => "Well written, write again soon!"}
+
 article.comments.create(comment)
+		if article.comments.create
+			puts "Article comment has been saved"
+
+		else
+			puts "could not be saved"
+
+		end
+else
+	puts "not saved"
+
+end
+# puts article.comments.errors.on 'comment_title'
+# puts article.comments.errors.on 'comment_body'
+
+
+#data validation with active record
+
