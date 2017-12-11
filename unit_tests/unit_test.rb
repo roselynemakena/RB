e7400c188352cb1301afbc403cc5f07de17eb978
Makena <roselynemakena@gmail.com>
2017-12-11 11:59:44 +0300

    #unit debugging

--- unit_tests/unit_test.rb
+++ unit_tests/unit_test.rb
@@ -1,21 +1,27 @@
 require 'test-unit'
+require 'breakpoint'
+require './unit_tests/unit.rb'
 
 class MTests < Test::Unit::TestCase
 	def setup
-		@student = Student.new("Roselyne", "Makena", 234)
+		@student = Student.new("Roselyne", "Makena", 6234)
 		
 	end
 
 
 	def test_first_name
 		assert_equal("Roselyne", @student.first_name)
+		puts "About to break.."
+		breakpoint
 	end
 
 	def test_last_name
-		
+		assert_equal("Makena", @student.last_name)	
 	end
 
 	def test_adm_no
+		assert_equal(6234, @student.adm_no)	
+
 		
 	end
 
